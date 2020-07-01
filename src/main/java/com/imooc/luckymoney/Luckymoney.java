bb118807f234cf9808e2c0c0b2fcd3c9996d02c3
DYaiu <daiyuss@126.com>
2020-07-01 13:57:34 +0800

    change pom.xml by jpa change application-dev.yml by mysql config add a mysql entity class

---
+++ src/main/java/com/imooc/luckymoney/Luckymoney.java
@@ -0,0 +1,50 @@
+package com.imooc.luckymoney;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+/**
+ * @author ljj
+ * @version 1.0
+ * @date 2020/7/1
+ */
+@Entity
+public class Luckymoney {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private BigDecimal money;
+
+    /**
+     * 发送人
+     */
+    private String producer;
+
+    /**
+     * 接收人
+     */
+    private String consumer;
+
+    public Luckymoney() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public String getConsumer() {
+        return consumer;
+    }
+}
